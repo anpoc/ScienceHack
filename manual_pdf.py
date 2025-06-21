7e0354af0b8a2ae834432c71bce92d2c9b9fce03
laloq11 <laloq11@gmail.com>
2025-06-21 17:24:40 +0000

    deletion of german invoices and adding more categories to check

--- manual_pdf.py
+++ manual_pdf.py
@@ -1,33 +1,16 @@
-import glob, re
+import glob, re,json
 import pymupdf
 from typing import Union
 from typing import List
-
-
-
-Bestell = ["Bestellnr.:","BestellNr.:", "Bestell.:"]
-Seite = [ "Seite 1", "Seite"]
-
-Datum = ["Datum:", "Datum","Lieferdatum"]
-Datum_str = [r'\b\d{2}.\d{2}.\d{4}\b',
-                r'\b\d{2}.\d{2}.\d{2}\b',
-                r'\b\d{2}/\d{2}/\d{4}\b',
-                r'\b\d{2}/\d{2}/\d{2}\b']
-
-Time = ["Uhr: ", "Time", "Time: "]
-main_page_types = {"Bestell": [Bestell,Bestell,None],
-                   "Datum": [Datum, Datum_str,None],
-                #    "Time": [["Time"], [r'\b\d{2}:\d{2}:\d{2}\b'],timecheck],
-                   "Seite": [Seite,Seite,None]
-                   }
-
+from tqdm import tqdm
 
 def timecheck(txt,main_page_types):
-    for i, line in enumerate(txt.splitlines()):
-        line = line.decode("utf-8")  # Convert bytes to str
-        t_match = re.search(line,main_page_types["Time"][1])
-        if t_match:
-            return True
+    # for i, line in enumerate(txt.splitlines()):
+    #     print(line)
+    #     line = line.decode("utf-8")  # Convert bytes to str
+    t_match = re.search(txt,main_page_types["Time"][1])
+    if t_match:
+        return True
     return False
 
 def type_match_check(line: str, type_str: Union[str, list[str]]) -> bool:
@@ -41,34 +24,66 @@ def type_match_check(line: str, type_str: Union[str, list[str]]) -> bool:
     else:
         raise TypeError(f"Expected str or list[str], got {type(type_str)}")
 
+def compare_Raul():
+    json_files = glob.glob("labels.json")  # or use a specific folder path
+
+    data_list = []
+    for file in json_files:
+        with open(file, 'r') as f:
+            data = json.load(f)
+            data_list.append(data)
+    sum = 0
+    fail=[]
+    for i in tqdm(range(len(data_list[0]))):
+        data = data_list[0][i]
+        totest = manual_readPDF("data/BECONEX_challenge_materials_samples"+data["path"][4:])
+        if data["label"] == totest[data["page"]]:
+            sum +=1
+        else:
+            fail.append(i)
+            # print('success! %d / %d '%(sum,i+1))
+    print("Failed at:",fail)
+    return 100 * sum / len(data_list[0])
+
 def type_check(txt,type2check,main_page_types,line_lim):
-    if main_page_types == None:
-        match_type_func = main_page_types[type2check][2]
-    else:
-        # print("type match check auto")
-        match_type_func = type_match_check
+    forcepass = False
+    t_match_name = False
+
+    # if main_page_types[type2check][2] == False:
+    #     match_type_func = type_match_check
+    # else:
+    #     match_type_func = main_page_types[type2check][2]
+    #     forcepass = True
+    #     t_match_name = True
+    match_type_func = type_match_check
+    if main_page_types[type2check][2]:
+        forcepass = True
+        t_match_name = True
+    # else:
+        # match_type_func = main_page_types[type2check][2]
 
     for i in range(line_lim):
         # print(i)
         line = txt.splitlines()[i]
         line = line.decode("utf-8")  # Convert bytes to str        
         # first checking if there is the keyword on the line...
-        for j in main_page_types[type2check][0]:
-        # print(type2check,j)
-            t_match_name = match_type_func(line,j)
-            if t_match_name:
-                # print("Match at %s using %s"%(type2check,j))
-                break
-
+        if not forcepass:
+            for j in main_page_types[type2check][0]:
+            # print(type2check,j)
+                t_match_name = match_type_func(line,j)
+                if t_match_name:
+                    # print("Match at %s using %s"%(type2check,j))
+                    break
         if t_match_name:
             # print("%s found, now looking for more..."%type2check)
             ## check if there is information that matches this info
-            for i2 in range(3):
-                line2 = txt.splitlines()[i+i2]
-                line2 = line2.decode("utf-8")  # Convert bytes to str
-                t_match2 = match_type_func(line2,main_page_types[type2check][1])
-                if t_match2:
-                    return True
+            if i+3 < len(txt.splitlines()):
+                for i2 in range(3):
+                    line2 = txt.splitlines()[i+i2]
+                    line2 = line2.decode("utf-8")  # Convert bytes to str
+                    t_match2 = match_type_func(line2,main_page_types[type2check][1])
+                    if t_match2:
+                        return True
     return False
 
 def isthismainpage(page,main_page_types,line_lim=30):
@@ -87,53 +102,67 @@ def isthismainpage(page,main_page_types,line_lim=30):
         print("no main page found...")
     return page_found
 
+
 def manual_readPDF(pdf_path: str)-> List[int]:
     assert pdf_path != ""
 
     doc = pymupdf.open(pdf_path) # open a document
-
-    text = doc[0].get_text()  # This is a string, not bytes
-    lines = text.splitlines()
-    num_lines = len(lines)
-    # print(num_lines)
-    len(doc) ## this gives the number of pages
-    main_data = ["Datum", "Seite 1"]
-    # print(len(doc))
-    pages_patch= {}
     main_page = []
-    current_main_page = 0
     main_page_b = False
     pages_test = []
-    errors=0
-    lines2check = 40
-    for n,page in enumerate(doc): # iterate the document pages
-
+    lines2check = 80
+    for page in doc: # iterate the document pages
         main_page = isthismainpage(page,main_page_types,lines2check)
-
+        print(main_page)
         if any(main_page):
             main_page_b = True
-
         else:
             main_page_b = False
- 
         pages_test.append(main_page_b)
+    
     pages_test=[int(b) for b in pages_test]
-
+    # print(pages_test)
     return pages_test
 
 
+
+
+
+Bestell = ["Bestellnr.:","BestellNr.:", "Bestell.:"]
+Seite = [ "Seite 1", "Seite"]
+Seite_str = [ "Seite 1", "1"]
+Address = ["Rechnungsadresse","Address","addresse"]
+Lieferschein = ["Lieferschein-Nr.", "Lieferschein"]
+
+Datum = ["Datum:", "Datum","Lieferdatum"]
+Datum_str = [r'\b\d{2}.\d{2}.\d{4}\b',
+                r'\b\d{2}.\d{2}.\d{2}\b',
+                r'\b\d{2}/\d{2}/\d{4}\b',
+                r'\b\d{2}/\d{2}/\d{2}\b']
+
+Time = ["Time", "Time: "]
+
+
+
+main_page_types = {
+                   "Datum": [Datum, Datum_str,False],
+                   "Seite": [Seite,Seite,False],
+                "Bestell": [Bestell,Bestell,False],
+                "Lieferschein":[Lieferschein,Lieferschein,False],
+                   "Time": [["Time"], [r'\b\d{2}:\d{2}:\d{2}\b'],True],
+                   "Address": [Address,Address,True]
+                   }
+
 if __name__ == "__main__":
-    papath = "data/"
-    pdf_files = glob.glob(papath+"*.pdf")
-    pdftouse = pdf_files[0]
-    # print(pdf_files)
-    print('\n\n\n')
+    papath = "data/BECONEX_challenge_materials_samples/"
+    pdfs = glob.glob(papath+"*.pdf")
+    pdftouse = pdfs[0]
     totest = manual_readPDF(pdftouse)
 
     print("The results using '%s' is: "%pdftouse)
     print(totest)
 
-    # print("The score wrt to Raul's list: {:.2f} %".format(compare_Raul()))
+    print("The score wrt to Raul's list: {:.2f} %".format(compare_Raul()))
 
     # for tt in pdf_files:
     #     totest = manual_readPDF(tt)
