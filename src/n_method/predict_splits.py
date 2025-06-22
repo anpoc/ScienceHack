e71205bad3768112763743d876d1f8cd782649de
raul <rauldavidds@hotmail.com>
2025-06-22 07:50:54 +0000

    nils_method

---
+++ src/n_method/predict_splits.py
@@ -0,0 +1,22 @@
+from id_match_rigid import predict as id_match_rigid_predict
+from large_texts import predict as large_texts_predict
+from page_number import predict as page_number_predict
+
+
+def predict(pdf_path: str):
+    id_rigid_pred = id_match_rigid_predict(pdf_path)
+    large_texts_pred = large_texts_predict(pdf_path)
+    page_number_pred = page_number_predict(pdf_path)
+    n = len(id_rigid_pred)
+    pred = [1] * n
+    for i in range(n):
+        if id_rigid_pred[i] == 0:
+            pred[i] = 0
+            continue
+        if large_texts_pred[i] == 0:
+            pred[i] = 0
+            continue
+        if page_number_pred[i] == 0:
+            pred[i] = 0
+            continue
+    return pred
